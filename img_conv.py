db10b8f417a398b6084023673a3abac9b63b2564
Lucas Schladitz <lsl@hup.de>
2025-08-07 15:01:52 +0200

    trying to outpt as RGB565

--- img_conv.py
+++ img_conv.py
@@ -1,8 +1,7 @@
 from PIL import Image
 
-image = Image.open("./img.png").convert("RGB")
-image = image.crop((0, 0, 320, 240))  # Ensure it's 320x240
-
+image = Image.open("img.png").convert("RGB")
+image = image.crop((0, 0, 320, 240))
 pixels = image.load()
 
 output = []
@@ -10,7 +9,18 @@ output = []
 for y in range(240):
     for x in range(320):
         r, g, b = pixels[x, y]
-        output.append(f"0x{r:02X}{g:02X}{b:02X}")
+
+        # Convert to RGB565
+        r5 = r >> 3
+        g6 = g >> 2
+        b5 = b >> 3
+
+        color = (r5 << 11) | (g6 << 5) | b5
+
+        # Swap bytes for little-endian .hword
+        color_le = ((color & 0xFF) << 8) | (color >> 8)
+
+        output.append(f"0x{color_le:04X}")
 
 with open("img.txt", "w") as f:
-    f.write(','.join(output))
+    f.write(", ".join(output))
